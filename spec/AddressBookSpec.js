f6e804685233d1e0de4c6ad47acd5bf510bc0a10
Valentinmagde <38159415+Valentinmagde@users.noreply.github.com>
2023-07-14 00:11:00 +0400

    completed course

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,10 +1,36 @@
 describe('Address Book', () => {
+    var addressBook,
+        thisContact;
+    beforeEach(() => {
+        addressBook = new AddressBook();
+        thisContact = new Contact();
+    });
+
     it('should be able to add a contact', function(){
-        var addressBook = new AddressBook(),
-            thisContact = new Contact();
-        
         addressBook.addContact(thisContact);
 
         expect(addressBook.getContact(0)).toBe(thisContact);
+    });
+
+    it('should be able to delete a contact', function(){
+        addressBook.addContact(thisContact);
+        addressBook.deleteContact(0);
+
+        expect(addressBook.getContact(0)).not.toBeDefined();
     })
 });
+
+describe('Async Address Book', () => {
+    var addressBook = new AddressBook();
+
+    beforeEach((done) => {
+        addressBook.getInitialContacts(() => {
+            done();
+        });
+    });
+
+    it('should grab initial contacts', (done) => {
+        expect(addressBook.initialComplete).toBe(true);
+        done();
+    });
+})
--- src/AddressBook.js
+++ src/AddressBook.js
@@ -1,7 +1,19 @@
 // var contacts = Array();
 function AddressBook(){
     this.contacts = [];
+    this.initialComplete = false;
 }
+
+AddressBook.prototype.getInitialContacts = function(cb) {
+    var self = this;
+
+    setTimeout(() => {
+        self.initialComplete = true;
+
+        if(cb)
+            return cb();
+    }, 3)
+};
 
 AddressBook.prototype.addContact = function(contact) {
     this.contacts.push(contact);
@@ -10,3 +22,7 @@ AddressBook.prototype.addContact = function(contact) {
 AddressBook.prototype.getContact = function(index) {
     return this.contacts[index];
 };
+
+AddressBook.prototype.deleteContact = function(index){
+    delete this.contacts[index];
+}
